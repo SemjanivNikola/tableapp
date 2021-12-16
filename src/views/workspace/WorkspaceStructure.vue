5a087d1110425f05bb8259e0998661221303c535
Alen Halilović <alenhalilovi490@gmail.com>
2021-12-16 22:30:34 +0100

    Header
    +

--- src/views/workspace/WorkspaceStructure.vue
+++ src/views/workspace/WorkspaceStructure.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="structure-wrapper">
-    <h1>WORKSPACE STRUCTURE</h1>
-  </div>
+        <div class="input-group">
+          <div id="container">
+            <img src="@/assets/add.svg" alt="">
+            <input type="search" class="form-control rounded" placeholder="  Search" aria-label="Search" aria-describedby="search-addon" />
+          </div>
+          <div id="structure">
+            <p>Structure</p>
+            <img src="@/assets/add.svg" alt="" class="icon">
+          </div>
+        </div>
+    </div>
 </template>
+
 
 <script>
 export default {
@@ -11,7 +21,29 @@ export default {
 </script>
 
 <style scoped>
+
 .structure-wrapper {
   width: 18%;
 }
+.form-control{ 
+  background-color: #211D43;
+  color: white;
+  width: 100%
+}
+#container {
+  display: flex;
+  padding: 5px;
+}
+#structure{
+  color: white;
+  text-decoration: underline;
+  text-decoration-color:#6BD1FF; 
+  display: flex;
+  align-items: flex-start;
+  padding: 10px;
+}
+.icon{
+  margin-left: auto;
+}
+
 </style>
