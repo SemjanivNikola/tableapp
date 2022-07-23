03a68c57cc66b49bbc1e58163025f6d426a4611a
Nikola <nikola.sem450@gmail.com>
2022-07-23 13:22:04 +0200

    Added table data rendering

--- src/views/table/TableView.vue
+++ src/views/table/TableView.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
     <table-action-bar />
+    <!-- <table-header :header="mockData.header" />
+    <table-body :body="mockData.body" /> -->
+
+    <table>
+      <thead>
+        <tr>
+          <th
+            v-for="(header, index) in mockData.header"
+            :key="index"
+          >
+            {{ header.text }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(row, rowIndex) in mockData.body"
+          :key="rowIndex"
+        >
+          <td
+            v-for="(cell, cellIndex) in row"
+            :key="cellIndex"
+          >
+            {{ cell.value }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
     TABLE
   </div>
 </template>
 
 <script>
+import tableData from "../../../mock-data/table_one.json";
 import TableActionBar from "./TableActionBar.vue";
 
 export default {
@@ -13,8 +43,14 @@ export default {
   components: { TableActionBar },
   data () {
     return {
-
+      mockData: tableData,
     };
   },
 };
 </script>
+
+<style scoped>
+th, td {
+  color: white;
+}
+</style>
