0f6d223ae74939a245eeb15485f6256e6b09aaae
Nikola <nikola.sem450@gmail.com>
2022-07-24 14:13:47 +0200

    Fix reactivity error - variable white defined in render

--- src/views/workspace/WorkspaceStructure.vue
+++ src/views/workspace/WorkspaceStructure.vue
@@ -1,36 +1,41 @@
 <template>
   <div class="structure-wrapper">
-        <div class="input-group">
-          <div id="container">
-            <icon
-              name="menu"
-              class="menu"
-              :size="16"
-              :color="white"
-      	    />
-            <input type="search" class="form-control rounded" placeholder="  Search" aria-label="Search" aria-describedby="search-addon" />
-          </div>
-          <div id="bar">
-            <p>Structure</p>
-            <icon
-              name="add"
-              class="add"
-              :size="20"
-              :color="white"
-      	    />
-          </div>
-          <div id="structure">
-            <icon
-              name="file"
-              class="file"
-              :size="16"
-              :color="white"
-      	    />
-          </div>
-        </div>
+    <div class="input-group">
+      <div id="container">
+        <icon
+          name="menu"
+          class="menu"
+          :size="16"
+          color="white"
+        />
+        <input
+          type="search"
+          class="form-control rounded"
+          placeholder="  Search"
+          aria-label="Search"
+          aria-describedby="search-addon"
+        >
+      </div>
+      <div id="bar">
+        <p>Structure</p>
+        <icon
+          name="add"
+          class="add"
+          :size="20"
+          color="white"
+        />
+      </div>
+      <div id="structure">
+        <icon
+          name="file"
+          class="file"
+          :size="16"
+          color="white"
+        />
+      </div>
     </div>
+  </div>
 </template>
-
 
 <script>
 export default {
@@ -43,7 +48,7 @@ export default {
 .structure-wrapper {
   width: 18%;
 }
-.form-control{ 
+.form-control{
   background-color: #211D43;
   color: white;
   width: 100%
@@ -55,7 +60,7 @@ export default {
 #bar{
   color: white;
   text-decoration: underline;
-  text-decoration-color:#6BD1FF; 
+  text-decoration-color:#6BD1FF;
   display: flex;
   align-items: flex-start;
   padding: 10px;
