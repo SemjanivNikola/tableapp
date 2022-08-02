727daaa9012d8c0fd814762a4b638a56555a9909
Nikola <nikola.sem450@gmail.com>
2022-08-02 18:27:38 +0200

    Added ViewTrash component

---
+++ src/components/ViewTrash.vue
@@ -0,0 +1,18 @@
+<template>
+  <div slot="header">
+    <h3>
+      Sort by
+    </h3>
+  </div>
+</template>
+
+<script>
+// import tableData from "../../mock-data/table_one.json";
+
+export default {
+    name: "ViewTrash",
+};
+</script>
+
+<style scoped>
+</style>
