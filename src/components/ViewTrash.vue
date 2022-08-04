d55a40edc0f37b616a2e8aa5ac6d9de60a5983b3
Nikola <nikola.sem450@gmail.com>
2022-08-04 09:31:32 +0200

    Added header in ViewTrash comp

--- src/components/ViewTrash.vue
+++ src/components/ViewTrash.vue
@@ -1,8 +1,24 @@
 <template>
-  <div slot="header">
-    <h3>
-      Sort by
-    </h3>
+  <div class="flex-row justify-between">
+    <div class="flex-row">
+      <h3>
+        Sort by
+      </h3>
+      <div class="spacer-md" />
+      <p>Restore items deleted from workspace in past seven days</p>
+    </div>
+    <div class="flex-row">
+      <button class="btn btn-fill">
+        Empty trash
+      </button>
+      <div class="spacer-md" />
+      <button
+        class="btn"
+        @click="$emit('close')"
+      >
+        X
+      </button>
+    </div>
   </div>
 </template>
 
@@ -15,4 +31,25 @@ export default {
 </script>
 
 <style scoped>
+p {
+  margin: 0;
+}
+.flex-row {
+  display: flex;
+  align-items: center;
+}
+.justify-between {
+  justify-content: space-between;
+}
+.spacer-md {
+  height: 16px;
+  width: 16px;
+}
+.btn {
+  padding: 8px;
+  cursor: pointer;
+}
+.btn-fill {
+  background-color: #DCDCDC;
+}
 </style>
