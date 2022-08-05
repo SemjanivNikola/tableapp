6f26e9c9448ca323b90e734dfb22805c486f196e
Nikola <nikola.sem450@gmail.com>
2022-08-05 15:12:46 +0200

    Default first rout updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/home/Home.vue";
+import Workspace from "../screens/Workspace.vue";
+// import Home from "../views/home/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: "/",
-        name: "Home",
-        component: Home,
+        name: "Workspace",
+        component: Workspace,
     },
     // {
     //   path: "/about",
