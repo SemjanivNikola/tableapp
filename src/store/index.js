b0b3fb2ec03a0ad32f4c76288c1182649960692d
Nikola <nikola.sem450@gmail.com>
2022-08-05 11:37:59 +0200

    Added workspace module to root store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import workspace from "./workspace";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-    },
-    mutations: {
-    },
-    actions: {
-    },
+    strict: process.env.NODE_ENV !== "production",
+    state: {},
+    mutations: {},
+    actions: {},
     modules: {
+        workspace: {
+            namespaced: true,
+            ...workspace,
+        },
     },
 });
