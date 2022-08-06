8c1a8978629cc8078f76a506fe9be1be4bc25d96
Nikola <nikola.sem450@gmail.com>
2022-08-06 19:33:26 +0200

    Added gridOptions module with hide fields fn

---
+++ src/store/gridOptions.js
@@ -0,0 +1,43 @@
+
+export default {
+    state: {
+        options: null,
+    },
+    mutations: {
+        set (state, payload) {
+            state.options = payload;
+        },
+        handleFieldVisibility (state, payload) {
+            if (payload.isHidden) {
+                const indexToRemove = state.options.hideFields.indexOf(payload.index);
+                state.options.hideFields.splice(indexToRemove, 1);
+            } else {
+                state.options.hideFields.push(payload.index);
+            }
+        },
+        addOptionToSummary (state, payload) {
+            state.options.summary.push(payload);
+        },
+        removeOptionFromSummary (state, payload) {
+            state.options.summary.splice(state.summary.indexOf(payload), 1);
+        },
+    },
+    getters: {
+        get: (state) => {
+            return state.options;
+        },
+    },
+    actions: {
+        firstFieldHidden ({ state, commit }) {
+            if (!state.options.summary.includes("hide_fileds")) {
+                commit("addOptionToSummary", "hide_fields");
+            }
+        },
+        lastFieldRemoved ({ state, commit }) {
+            if (state.options.hideFields.length === 0) {
+                commit("removeOptionFromSummary", "hide_fields");
+            }
+        },
+
+    },
+};
