236b797dd501959770f7551f8d92854e351cbf2d
Nikola <nikola.sem450@gmail.com>
2022-08-11 12:37:29 +0200

    Template for adding new record

--- src/views/table/TableRecordList.vue
+++ src/views/table/TableRecordList.vue
@@ -1,6 +1,9 @@
 <template>
     <tbody>
         <record-list-item v-for="record in recordList" :key="record.id" :record="record" />
+        <tr class="new-record" @click="$emit('onRecordCreate')">
+            <td colspan="100%">+ Add new record</td>
+        </tr>
       </tbody>
 </template>
 
@@ -18,3 +21,19 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.new-record {
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+    background-color: rgba(255, 255, 255, 0.3);
+}
+td {
+    padding-left: 8px;
+    text-align: left;
+    font-size: 14px;
+    color: white;
+    border: none;
+}
+</style>
--- src/views/table/TableView.vue
+++ src/views/table/TableView.vue
@@ -1,30 +1,47 @@
 <template>
-    <div style="overflow-x: scroll;">
-      <table-action-bar />
+  <div style="overflow-x: scroll">
+    <table-action-bar />
 
       <table>
         <header-list :header="tableHeader" />
-        <table-record-list :record-list="getRecordList" />
+        <table-record-list
+          :record-list="getRecordList"
+          @onRecordCreate="onRecordCreate"
+        />
       </table>
-    </div>
+    <modal-wrapper :is-shown="isRecordExpandShown">
+        <record-expand
+            :fields="tableHeader"
+            @close="isRecordExpandShown = false"
+        />
+    </modal-wrapper>
+  </div>
 </template>
 
 <script>
+import ModalWrapper from "@/components/ModalWrapper.vue";
 import { mapState } from "vuex";
 import HeaderList from "./HeaderList.vue";
 import TableActionBar from "./TableActionBar.vue";
 import TableRecordList from "./TableRecordList.vue";
+import RecordExpand from "./RecordExpand.vue";
 
 export default {
     name: "TableView",
-    components: { TableActionBar, HeaderList, TableRecordList },
+    components: { TableActionBar, HeaderList, TableRecordList, ModalWrapper, RecordExpand },
     data () {
         return {
             tableHeader: [],
+            isRecordExpandShown: false,
         };
     },
     created () {
         this.tableHeader = this.$store.getters["view/getHeader"];
+    },
+    methods: {
+        onRecordCreate () {
+            this.isRecordExpandShown = true;
+        },
     },
     computed: {
         ...mapState(["view"]),
@@ -37,13 +54,15 @@ export default {
 
 <style scoped>
 table {
-    border-collapse: collapse;
+  border-collapse: collapse;
 }
-th, td {
+th,
+td {
   border: 1px solid #fff;
   color: white;
 }
-th.hidden, td.hidden {
+th.hidden,
+td.hidden {
   display: none;
 }
 </style>
