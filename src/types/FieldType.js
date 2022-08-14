f1d1d204cf0ea0eee0f8f3e74124347d4ca284bc
Nikola <nikola.sem450@gmail.com>
2022-08-14 10:42:01 +0200

    Typo fix on field type type

--- src/types/FieldType.js
+++ src/types/FieldType.js
@@ -6,7 +6,7 @@ const FieldType = [
     { id: 5, name: "checkbox", type: "text" },
     { id: 6, name: "select", type: "text" },
     { id: 7, name: "date", type: "number" },
-    { id: 8, name: "emal", type: "text" },
+    { id: 8, name: "email", type: "text" },
     { id: 9, name: "duration", type: "number" },
     { id: 10, name: "created at", type: "number" },
     { id: 11, name: "last modified at", type: "number" },
